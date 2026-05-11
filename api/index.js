880471bd3ad4b9665a0b842642b43fdf6e5ee663
tevdoskiart <m.tevdoski@gmail.com>
2026-05-11 09:25:34 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,34 +1,8 @@
-javascriptconst express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-const CLIENT_KEY = process.env.TIKTOK_CLIENT_KEY;
-const CLIENT_SECRET = process.env.TIKTOK_CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-
-app.get('/auth/tiktok', (req, res) => {
-  const url = `https://www.tiktok.com/v2/auth/authorize?client_key=${CLIENT_KEY}&response_type=code&scope=video.upload&redirect_uri=${REDIRECT_URI}`;
+javascriptexport default function handler(req, res) {
+  const CLIENT_KEY = process.env.TIKTOK_CLIENT_KEY;
+  const REDIRECT_URI = process.env.REDIRECT_URI;
+  
+  const url = `https://www.tiktok.com/v2/auth/authorize?client_key=${CLIENT_KEY}&response_type=code&scope=video.upload&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
+  
   res.redirect(url);
-});
-
-app.get('/auth/callback', async (req, res) => {
-  const { code } = req.query;
-  try {
-    const response = await axios.post('https://open.tiktokapis.com/v2/oauth/token/', {
-      client_key: CLIENT_KEY,
-      client_secret: CLIENT_SECRET,
-      code,
-      grant_type: 'authorization_code',
-      redirect_uri: REDIRECT_URI
-    });
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.listen(3000, () => console.log('Server running on port 3000'));
+}
